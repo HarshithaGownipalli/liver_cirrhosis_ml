504e94534d3a7c2f084b44627297512da09d3111
HarshithaGownipalli <harshithagownipalli@gmail.com>
2025-07-20 14:54:14 +0530

    Add files via upload

---
+++ Project Files/Flask/app.py
@@ -0,0 +1,37 @@
+import os
+import pickle
+import numpy as np
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Absolute path to current directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Load scaler and model
+with open(os.path.join(basedir, 'sc.pkl'), 'rb') as f:
+    sc = pickle.load(f)
+
+with open(os.path.join(basedir, 'model.pkl'), 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    inputs = [float(x) for x in request.form.values()]
+    inputs = np.array([inputs])
+    inputs = sc.transform(inputs)
+    output = model.predict(inputs)
+    print("Raw model prediction:", output)
+    prediction = int(output[0])
+
+    print("Input Data:", inputs)
+    print("Prediction:", prediction)
+
+    return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
